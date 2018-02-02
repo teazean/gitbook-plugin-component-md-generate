4954d7ed2fd94b36bbbdb94cac3bf95d857d9b6b
zhanglei55 <zhanglei55@baidu.com>
2018-02-02 19:33:13 +0800

    update

--- replacer/version.js
+++ replacer/version.js
@@ -3,5 +3,12 @@ const {
     getBookDir
 } = require('../helper');
 module.exports = function (page, config) {
-
+    try {
+        const version = require(path.join(getBookDir(), config.packageJsonPath)).version;
+        page.content = page.content.replace(/\{\{VERSION}}/g, version);
+    }
+    catch (e) {
+        // nothing
+        console.error(e);
+    }
 }
